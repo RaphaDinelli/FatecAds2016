dcf2e29fb7c3db61689ae727af6ea15710a9fdad
Raphael Dinelli <raphael.dinelli14@gmail.com>
2016-05-24 18:52:26 -0300

    Update caixa_eletronico.py

--- caixa_eletronico.py
+++ caixa_eletronico.py
@@ -16,119 +16,118 @@ CAIXA ELETRÔNICO
 1.8.4. Valor das sobras dos caixas.
 '''
 
-
 caixa = [
-			 (100,50,20,10,5,2), 
-			 [0,0,0,0,0,0], 
-			 [0,0,0,0,0,0]
-		 ]
+    (100, 50, 20, 10, 5, 2),
+    [0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0]
+]
 bancos = [
-			("Banco do Brasil","Santander", "Itaú", "Caixa"), 
-			[0,0,0,0],
-			[0,0,0,0],
-			[0,0,0,0]
-		]
-		
+    ("Banco do Brasil", "Santander", "Itaú", "Caixa"),
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
+    [0, 0, 0, 0]
+]
+
 limiteSaques = 100
 saldo_caixa = 0
 
+
 def limpa_tela():
-	print("\n" * 100)
-	
+    print("\n" * 100)
+
+
 def mostrar_notas(x):
-	if x > 0:
-		for i in range(len(caixa[0])):
-			print((i+1), " -- R$", caixa[0][i])
-	else:
-		for i in range(len(caixa[0])):
-			if caixa[1][i] > 0:
-				print((i+1), " -- R$", caixa[0][i])
-				
+    if x > 0:
+        for i in range(len(caixa[0])):
+            print((i + 1), " -- R$", caixa[0][i])
+    else:
+        for i in range(len(caixa[0])):
+            if caixa[1][i] > 0:
+                print((i + 1), " -- R$", caixa[0][i])
+
+
 def saldo_total():
-	global saldo_caixa
-	for i in range(len(caixa[0])):
-		saldo_caixa = saldo_caixa + (caixa[0][i] * caixa[1][i])
-				
+    global saldo_caixa
+    for i in range(len(caixa[0])):
+        saldo_caixa = saldo_caixa + (caixa[0][i] * caixa[1][i])
+
+
 def carregar_notas():
-	print("CARREGAR NOTAS\n\n")
-	for i in range(len(caixa[0])):
-		print("Digite a quantidade de notas de R$%i" %(caixa[0][i]))
-		caixa[1][i] = int(input())
-		limpa_tela()
-		saldo_total()
-		print("Saldo do caixa: R$%s\n" %saldo_caixa )
-		for j in range(len(caixa[0])):
-			if caixa[1][j] > 0:
-				print("Nota de R$", caixa[0][j], "--- Quantidade: ",caixa[1][j])
-		print("\n")
-		
+    print("CARREGAR NOTAS\n\n")
+    for i in range(len(caixa[0])):
+        print("Digite a quantidade de notas de R$%i" % (caixa[0][i]))
+        caixa[1][i] = int(input())
+        limpa_tela()
+        saldo_total()
+        print("Saldo do caixa: R$%s\n" % saldo_caixa)
+        for j in range(len(caixa[0])):
+            if caixa[1][j] > 0:
+                print("Nota de R$", caixa[0][j], "--- Quantidade: ", caixa[1][j])
+        print("\n")
+
+
 def copia(x):
-	if x > 0:
-		for i in range(len(caixa[1])):
-			caixa[1][i] = caixa[2][i]
-	else:
-		for i in range(len(caixa[1])):
-			caixa[2][i] = caixa[1][i]
-	saldo_total()
-			
-		
-		
+    if x > 0:
+        for i in range(len(caixa[1])):
+            caixa[1][i] = caixa[2][i]
+    else:
+        for i in range(len(caixa[1])):
+            caixa[2][i] = caixa[1][i]
+    saldo_total()
+
+
 def validarSaque(valor1):
-	saldo_total()
-	valor = valor1
-	copia(0)
-	if valor1 > saldo_caixa:
-		print("EXCEDEU O LIMITE DO CAIXA!")
-		return False
-	while valor < 0:
-		if valor == 11:
-			caixa[3][4] == caixa[3][4] - 1
-			caixa[3][5] == caixa[3][5] - 3
-			valor := valor - 11
-			break
-		elif valor == 13:
-			caixa[3][4] == caixa[3][4] - 1
-			caixa[3][4] == caixa[3][4] - 4
-			valor := valor - 13
-			break
-		else:
-			for i in range(len(caixa[0])):
-				while valor > caixa[0][i]:
-					if caixa[3][i] > 0:
-						caixa[3][i] = caixa[3][i] - 1
-						valor = valor - caixa[0][i]
-								
-			
-				
-				
-	
-	
+    saldo_total()
+    valor = valor1
+    copia(0)
+    if valor1 > saldo_caixa:
+        print("EXCEDEU O LIMITE DO CAIXA!")
+        return False
+    while valor > 0:
+        if valor == 11:
+            caixa[3][4] = caixa[3][4] - 1
+            caixa[3][5] = caixa[3][5] - 3
+            valor = valor - 11
+            break
+        elif valor == 13:
+            caixa[3][4] = caixa[3][4] - 1
+            caixa[3][4] = caixa[3][4] - 4
+            valor = valor - 13
+            break
+        else:
+            cont = 0
+            for i in range(len(caixa[0])):
+                while valor > caixa[0][cont]:
+                    if caixa[3][cont] > 0:
+                        caixa[3][cont] = caixa[3][cont] - 1
+                        valor = valor - caixa[0][cont]
+                    else:
+                        if i < len(caixa[1]):
+                            break
+                        else:
+                            continue
+                        
+    def menu():
+        print("------------ CAIXA ELETRÔNICO ------------\n\n")
+        print("\tMenu Principal\n")
+        print("1 - Carregar notas")
+        print("2 - Retirar notas")
+        print("3 - Estatísticas")
+        print("9 - Fim\n")
 
+        opc = int(input("\nDigite a opção desejada: "))
 
+        while opc != 9:
+            if opc == 1:
+                carregar_notas()
+            elif opc == 2:
+                retirar_notas()
+            elif opc == 3:
+                estatisticas()
 
+        print("\n" * 100)
+        print("CAIXA ELETRONICO FINALIZADO")
 
-
-def menu():
-	print("------------ CAIXA ELETRÔNICO ------------\n\n")
-	print("\tMenu Principal\n")
-	print("1 - Carregar notas")
-	print("2 - Retirar notas")
-	print("3 - Estatísticas")
-	print("9 - Fim\n")
-	
-	opc = int(input("\nDigite a opção desejada: "))
-	
-	while opc != 9:
-		if opc == 1:
-			carregar_notas()
-		elif opc == 2:
-			retirar_notas()
-		elif opc == 3:
-			estatisticas()
-	
-	print("\n" * 100)
-	print("CAIXA ELETRONICO FINALIZADO")
-	
 carregar_notas()
 saldo_total()
-print(saldo_caixa)
+
