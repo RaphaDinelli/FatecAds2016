aafec948ee6e3a8377833f2e5a5583c5019ecfa3
Raphael Dinelli <raphael.dinelli14@gmail.com>
2016-07-19 01:47:44 -0300

    Update caixa_de_bar.py

--- caixa_de_bar.py
+++ caixa_de_bar.py
@@ -1,51 +1,74 @@
-'''Caixa de bar'''
-from datetime import datetime
+import datetime
+from time import sleep
 
-class Controle_financeiro(object):
-    # Armazena como string a data atual no formato aaaa/mm/dd
-    hoje = datetime.today()
+receitas = []
+despesas = []
+
+def menu():
+    opcoes = {1:"Cadastrar receitas", 2:"Cadastrar despesas", 3:"Resumo"}
+    print("\n" * 50)
+    print("$$$$ MENU DE OPÇÕES $$$$\n\n")
     
-    def __init__(self):
-        self.receitas = []
-        self.despesas = []
-        
-    def gerar_datas(self, data):
-        data=data.split("/")
-        data = [int(x) for x in data]
-        data = datetime(data[3],data[2],data[1])
-        return data
-        
-    def adicionar_receitas(self, tipo, valor, data=self.hoje):
-        self.receitas.append((tipo, valor, data))
-        
-    def adicionar_despesas(self, tipo, nome, valor, vencimento):
-        self.despesas.append((tipo, nome, valor, vencimento))
-        
-    def resumo(self, periodo_inicial, periodo_final):
-        total_receitas = 0.
-        total_despesas = 0.
-        maior_receita = 0.
-        maior_despesa = 0.
+    for i in opcoes:
+        print("%s - %s" %(i, opcoes[i]))
+    print()
+    
+    opcao = int(input("Digite a opçao desejada: "))
+    print("\n" * 50)
+    
+    if opcao == 1:
+        interface_adicionar_receitas()
 
-        for i in self.receitas:
-            if i[2] >= periodo_inicial and i[2] <= periodo_final:
-                total_receitas += i[1]
-            if i[1] > maior_receita:
-                maior_receita = i[1]
-        for i in self.despesas:
-            if i[3] >= periodo_inicial and i[4] <= periodo_final:
-                total_despesas += i[2]
-            if i[2] > maior_despesa:
-                maior_despesa = i[1]    
-                
-        print("TOTAL DE RECEITAS: R$%.2f\n" %total_receitas)
-        print("TOTAL DE DESPESAS: R$%.2f\n\n" %total_despesas)
-        print("Maior receita: R$%.2f" %maior_receita)
-        print("Maior despesa: R$%.2f" %maior_receita)
-        
-        input("\nDigite enter para voltar ao menu.\n")
-        self.menu()
-        
-        
-    def menu(self):
-        pass
+def gerar_data(data):
+    data_int = data.split("/")
+    data_int = [int(x) for x in data_int]
+    data_data = datetime.datetime(data_int[2], data_int[1], data_int[0])
+    return data_data
+    
+def adicionar_receitas(tipo, valor, data):
+    receitas.append((tipo, valor, gerar_data(data)))
+    
+def interface_adicionar_receitas():
+    tipos = ["Salário", "Vale refeição", "Comissão", "Outros"]
+    print("+++ ADICIONAR RECEITAS +++\n\n")
+    try:
+        opcao = int(input("Digite 1 para adicionar receitas e 2 para adicionar tipos: "))
+    except :
+        print("Caractere digitado invalido! Tente outra vez\n.")
+        interface_adicionar_receitas()
+    if opcao == 1:
+        print("Tipos de receitas: \n")
+        for i in tipos:
+            print(i)
+        try:
+            tipo = input("\nDigite o tipo de receita: ")
+            valor = float(input("Digite o valor: "))
+            data = input("Digite a data no formato (dd/mm/aaaa)")
+        except:
+        	print("Dados inconsistentes! Tente outra vez.")
+        	interface_adicionar_receitas()
+        adicionar_receitas(tipo, valor, data)
+        print("Operação concluida!")
+        sleep(5)
+        menu()
+    
+def adicionar_despesas(nome, tipo, valor, vencimento):
+    receitas.append((nome, tipo, valor, gerar_data(vencimento)))
+
+def resumo(periodo):
+    total_receitas = 0.
+    total_despesas = 0.
+    for i in receitas:
+        if i[2] >= periodo:
+            total_receitas += i[1]
+            
+    for i in despesas:
+         if i[3] >= periodo:
+             total_despesas += i[2]
+    
+    print("RESUMO\n\n")
+    print("Total das receitas: R$%.2f" % total_receitas)
+    print("Total das receitas: R$%.2f\n" % total_receitas)
+    print("Saldo: R$%.2f" % (total_receitas - total_despesas))
+    
+menu()
