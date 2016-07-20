d155cdf1e452454d5203de1ba95d015833faf383
Raphael Dinelli <raphael.dinelli14@gmail.com>
2016-07-20 05:00:41 -0300

    Update caixa_de_bar.py

--- caixa_de_bar.py
+++ caixa_de_bar.py
@@ -29,28 +29,47 @@ def adicionar_receitas(tipo, valor, data):
     receitas.append((tipo, valor, gerar_data(data)))
     
 def interface_adicionar_receitas():
-    tipos = ["Salário", "Vale refeição", "Comissão", "Outros"]
+    tipos = {1:"Salário", 2:"Vale refeição", 3:"Comissão", 4:"Outros"}
     print("+++ ADICIONAR RECEITAS +++\n\n")
     try:
         opcao = int(input("Digite 1 para adicionar receitas e 2 para adicionar tipos: "))
     except :
         print("Caractere digitado invalido! Tente outra vez\n.")
+        input("\nTecle enter para continuar.")
         interface_adicionar_receitas()
     if opcao == 1:
         print("Tipos de receitas: \n")
         for i in tipos:
-            print(i)
+            print("%s - %s" %(i, tipos[i]))
         try:
-            tipo = input("\nDigite o tipo de receita: ")
+            tipo = int(input("\nDigite o tipo de receita: "))
+            while tipo not in tipos:
+            	print("\n"*50+"OPCÃO INVALIDA!\n")
+            	for i in tipos:
+            		print("%s - %s" %(i, tipos[i]))
+            	tipo = int(input("\nDigite o tipo de receita: "))
             valor = float(input("Digite o valor: "))
             data = input("Digite a data no formato (dd/mm/aaaa)")
         except:
         	print("Dados inconsistentes! Tente outra vez.")
         	interface_adicionar_receitas()
-        adicionar_receitas(tipo, valor, data)
-        print("Operação concluida!")
-        sleep(5)
+        adicionar_receitas(tipos[tipo], valor, data)
+        print("Operação concluida!\n")
+        input("Tecle enter para continuar.")
         menu()
+    elif opcao == 2:
+    	novo_tipo = input("Digite o novo tipo de receita: ")
+    	codigo_do_tipo = 0 
+    	for i in tipos:
+    		if codigo_do_tipo < i:
+    			codigo_do_tipo = i
+    	codigo_do_tipo += 1
+    	tipos[codigo_do_tipo] = novo_tipo
+    	print("\nOperação concluida!\n")
+    	for i in tipos:
+    		print("%s - %s" %(i, tipos[i]))
+    	input("\nTecle enter para continuar.")
+    	menu()
     
 def adicionar_despesas(nome, tipo, valor, vencimento):
     receitas.append((nome, tipo, valor, gerar_data(vencimento)))
